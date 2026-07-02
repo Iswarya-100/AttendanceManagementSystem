86065bda83287f527b109e49d6a208d65ffc41e2
Iswarya-100 <iswarya01032007@gmail.com>
2026-07-02 22:56:53 +0530

    recorded presentees

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template, request
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+
+STUDENTS = pd.read_csv("data/students.csv").to_dict(orient="series")
+RNOS = STUDENTS["Roll Number"].values
+NAMES = STUDENTS["Student Name"].values
+nos = []
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return "hello world, I am Iswarya"
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def take_attendance():
+    return render_template("take_attendance.html", NAMES=NAMES, RNOS=RNOS)
+    
+    
+@app.route("/get_details", methods=["GET", "POST"])
+def get_details():
+    no = request.form.get("no")
+    if no:
+        nos.append(no)
+    if request.form.get("is_complete") == "complete":
+        return render_template("report.html", nos=nos)
+    return render_template("take_attendance.html", NAMES=NAMES, RNOS=RNOS)
+    
