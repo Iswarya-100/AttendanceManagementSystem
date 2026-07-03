a72d1cb81a843cf2138f25a614cad2e60fc9a09a
Iswarya-100 <iswarya01032007@gmail.com>
2026-07-03 21:26:16 +0530

    added css

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -21,15 +22,21 @@ def index():
 
 @app.route("/", methods=["POST"])
 def take_attendance():
-    return render_template("take_attendance.html", NAMES=NAMES, RNOS=RNOS)
-    
-    
-@app.route("/get_details", methods=["GET", "POST"])
+    return render_template("t_take_attendance.html", NAMES=NAMES, RNOS=RNOS)
+
+@app.route("/get_details", methods=["POST"])
 def get_details():
-    no = request.form.get("no")
-    if no:
-        nos.append(no)
-    if request.form.get("is_complete") == "complete":
-        return render_template("report.html", nos=nos)
-    return render_template("take_attendance.html", NAMES=NAMES, RNOS=RNOS)
+    attendance = json.loads(request.form["is_present"])
+    nos = [key for key, value in attendance.items() if value == False]
+    return render_template("report.html", nos=nos)
+    
+    
+# @app.route("/get_details", methods=["GET", "POST"])
+# def get_details():
+#     no = request.form.get("no")
+#     if no:
+#         nos.append(no)
+#     if request.form.get("is_complete") == "complete":
+#         return render_template("report.html", nos=nos)
+#     return render_template("t_take_attendance.html", NAMES=NAMES, RNOS=RNOS)
     
